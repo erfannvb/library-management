1fab8230e9951df5b980df7935de02c81caa1d10
Erfan <erfannavab@gmail.com>
2022-11-14 16:06:07 +0330

    added security config.

--- src/main/java/nvb/springframework/librarymanagement/config/SecurityConfig.java
+++ src/main/java/nvb/springframework/librarymanagement/config/SecurityConfig.java
@@ -1,12 +1,33 @@
 package nvb.springframework.librarymanagement.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test1234").roles("USER"))
+                .withUser(users.username("erfan").password("test1234").roles("USER", "ADMIN"));
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").loginProcessingUrl("/index")
+                .permitAll().and()
+                .logout().permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/access-denied");
+    }
 
 }
